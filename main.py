3de93cf58f7a017d22d29b22e47c011daff47560
Sauradeep Paul <sauradeep.paul@gmail.com>
2018-12-14 16:49:35 -0500

    Colors

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 from shapes import *
 from dimensions import *
 from game_strings import *
+from colors import *
 
 # creating the data structure for pieces
 # setting up global vars
@@ -32,10 +33,10 @@ class Piece(object):
 
 
 def create_grid(locked_positions={}):
-    #Setting color to black
-    grid = [[(0, 0, 0) for _ in range(PLAYZONE_WIDTH/BLOCK_SIZE)] for _ in range(PLAYZONE_HEIGHT/BLOCK_SIZE)]
+    # Setting color to black
+    grid = [[PLAYZONE_COLOR for _ in range(PLAYZONE_WIDTH/BLOCK_SIZE)] for _ in range(PLAYZONE_HEIGHT/BLOCK_SIZE)]
 
-    #Setting locked blocks
+    # Setting locked blocks
     # TODO: Check if this works
     # for i in range(len(grid)):
     #     for j in range(len(grid[i])):
@@ -65,14 +66,6 @@ def draw_text_middle(text, size, color, surface):
 
 
 def draw_grid(surface, grid):
-    surface.fill((0, 0, 0))
-
-    pygame.font.init()
-    font = pygame.font.SysFont(game_font, 60)
-    title = font.render(game_name, 1, (255, 255, 255))
-
-    surface.blit(title, TOP_LEFT_X + (WINDOW_WIDTH - title.get_width())/2, TITLE_PADDING_Y)
-
     for i in range(len(grid)):
         for j in range(len(grid)):
             pygame.draw.rect(surface,
@@ -80,6 +73,7 @@ def draw_grid(surface, grid):
                              (TOP_LEFT_X + j*BLOCK_SIZE, TOP_LEFT_Y + i*BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE),
                              0)
 
+    pygame.draw.rect(surface, BORDER_COLOR, (TOP_LEFT_X, TOP_LEFT_Y, PLAYZONE_WIDTH, PLAYZONE_HEIGHT), BORDER_SIZE)
 
 
 def clear_rows(grid, locked):
@@ -90,8 +84,17 @@ def draw_next_shape(shape, surface):
     pass
 
 
-def draw_window(surface):
-    pass
+def draw_window(surface, grid):
+    surface.fill(BACKGROUND_COLOR)
+
+    pygame.font.init()
+    font = pygame.font.SysFont(game_font, 60)
+    title = font.render(game_name, 1, TITLE_COLOR)
+
+    surface.blit(title, TOP_LEFT_X + (WINDOW_WIDTH - title.get_width())/2, TITLE_PADDING_Y)
+
+    draw_window(surface, grid)
+    pygame.display.update()
 
 
 def main():
